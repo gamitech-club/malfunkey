31a076a08f52ec4cc37afcefa34bb14a822628aa
Khairi <khairinajahrifky12@gmail.com>
2025-05-25 06:57:53 +0700

    Implement idle animation

--- Assets/Scripts/PlayerVisual.cs
+++ Assets/Scripts/PlayerVisual.cs
@@ -4,6 +4,8 @@ using EditorAttributes;
 public class PlayerVisual : MonoBehaviour
 {
     [SerializeField, Required] private Player _player;
+    [SerializeField, Required] private Animator _animator;
+    [SerializeField, Required] private SpriteRenderer _sprite;
 
     [Header("SFXs")]
     [SerializeField] private AudioSource _sfxJump;
@@ -16,6 +18,26 @@ public class PlayerVisual : MonoBehaviour
     private void OnDisable()
     {
         _player.Jumped -= OnJumped;
+    }
+
+    private void Update()
+    {
+        HandleAnimation();
+        HandleSpriteFlipping();
+    }
+
+    private void HandleAnimation()
+    {
+        _animator.Play("PlayerIdle");
+    }
+
+    private void HandleSpriteFlipping()
+    {
+        var moveInput = _player.MoveInput;
+        if (moveInput.x < 0)
+            _sprite.flipX = true;
+        else if (moveInput.x > 0)
+            _sprite.flipX = false;
     }
 
     private void OnJumped()
